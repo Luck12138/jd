6b2e42648038d30c0445b2d1940ac9e2910ed84e
Luck12138 <1623082663@qq.com>
2023-04-21 09:07:00 +0800

    update shop

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,24 @@
 import axios from 'axios'
 
+const instance = axios.create({
+  baseURL: 'https://www.fastmock.site/mock/b4c5bbf7102eaf8e787be441a79ecf1e/jingdong',
+  timeout: 10000
+})
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.get(url, params, {
+    }).then((response) => {
+      resolve(response.data)
+    }, err => {
+      reject(err)
+    })
+  })
+}
+
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
-      baseURL: 'https://www.fastmock.site/mock/b4c5bbf7102eaf8e787be441a79ecf1e/jingdong',
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    instance.post(url, data, {
     }).then((response) => {
       resolve(response.data)
     }, err => {
---
+++ src/views/shop/Shop.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <div class="search__back iconfont"
+      @click="handleBack"
+      >&#xe7e0;</div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont">&#xe64c;</span>
+        <input class="search__content__input" placeholder="请输入商品名称搜索" />
+      </div>
+    </div>
+    <ShopInfo :item='item' :hideBorder="true"/>
+  </div>
+</template>
+
+<script>
+// import { ref } from 'vue'
+// import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo.vue'
+import { useRouter } from 'vue-router'
+
+// const useNearbyListEffect = () => {
+//   const nearbyList = ref([])
+//   const getNearbyList = async () => {
+//     const result = await get('/api/shop/hot-list')
+//     if (result?.error === 0 && result?.data?.length) {
+//       nearbyList.value = result.data
+//     }
+//   }
+//   return { nearbyList, getNearbyList }
+// }
+
+const usehandlebackEffect = () => {
+  const router = useRouter()
+  const handleBack = () => {
+    router.push({ name: 'Home' })
+  }
+  return { handleBack }
+}
+
+export default {
+  name: 'Shop',
+  components: { ShopInfo },
+  setup () {
+    // const { nearbyList, getNearbyList } = useNearbyListEffect()
+    // getNearbyList()
+    const item = {
+      id: '1',
+      name: '沃尔玛',
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      sales: 10000,
+      expressLimit: 0,
+      expressPrice: 5,
+      slogan: 'VIP尊享满89元减4元运费劵'
+    }
+    const { handleBack } = usehandlebackEffect()
+    return { item, handleBack }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.wrapper {
+  padding: 0 .18rem;
+}
+.search {
+  line-height: .32rem;
+  display: flex;
+  margin: .16rem 0 .16rem 0;
+  &__back {
+    font-size: .24rem;
+    color:  #B6B6B6;
+    width: .3rem;
+  }
+  &__content {
+    display: flex;
+    flex: 1;
+    background: #F5F5F5;
+    border-radius: .16rem;
+    &__icon {
+      width: .44rem;
+      height: .32rem;
+      text-align: center;
+      color:  #B7B7B7;
+      padding-top: .015rem;
+    }
+    &__input {
+      display: block;
+      padding-right: .2rem;
+      border: none;
+      outline: none;
+      background: none;
+      height: .32rem;
+      width: 100%;
+      color: #333333;
+      font-size: 14px;
+      &::placeholder {
+        font-size: 14px;
+        color: #333333;
+        line-height: 16px;
+      }
+    }
+  }
+}
+</style>
